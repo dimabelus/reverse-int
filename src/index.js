08d3eb6e2acac0116115a65d9a39f5f40a5c452d
dimabelus <dima21belus@gmail.com>
2021-03-08 22:15:12 +0300

    feat: implement Reverse Int algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    // Create the absolute value of a number
+    let absN = Math.abs(n);
+    // Convert number to string
+    // Split string to character, reverse characters' order and join to string
+    let strNumber = absN.toString().split("").reverse().join("");
+    // Convert string to number
+    let convertedNumber = parseInt(strNumber);
+    // Return reversed number;
+    return convertedNumber;
+};
